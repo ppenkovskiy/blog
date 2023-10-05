7d33c5a782b7850abcf174c34b68ca79f17f101e
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-10-05 21:49:37 +0300

    c12

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,22 @@
+from datetime import date
+
 from django.shortcuts import render
+
+posts = [
+    {
+        'slug': 'hike-in-the-mountains',
+        'image': 'mountains.jpg',
+        'author': 'Me',
+        'date': date(2023, 7, 21),
+        'title': 'Mountain Hiking',
+        'excerpt': "There's nothing like the views you get when hiking in the mountains!",
+        'content': """
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad asperiores atque corporis culpa deleniti,
+        deserunt distinctio, dolor dolore eius fugit harum labore maxime nisi perferendis quae quis ratione
+        vel. Quam.
+        """
+    }
+]
 
 
 def starting_page(request):
