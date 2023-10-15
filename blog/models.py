ed330b00ba00580d397276e734046aa0e1efec47
Pavel Penkovskiy <p.penkovskiy@gmail.com>
2023-10-15 14:02:03 +0300

    c20

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,21 @@ from django.db import models
 from django.utils.text import slugify
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.caption}"
+
+
 class Post(models.Model):
     title = models.CharField(max_length=20)
     excerpt = models.CharField(max_length=100)
@@ -9,6 +24,8 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
     content = models.CharField(max_length=1000)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name='posts')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, null=True, related_name='posts')
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
@@ -16,4 +33,7 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.title}"
+
+
+
 
