3bc207e3e4f5aed13f3fa5258b296ec1d1d82ac0
Your Name <you@example.com>
2023-10-23 16:31:50 +0300

    24

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.StartingPageView.as_view(), name='starting-page'),
-    path("posts/", views.PostsView.as_view(), name='posts-page'),
+    path("posts/", views.AllPostsView.as_view(), name='posts-page'),
     path("posts/<slug:slug>", views.ReviewDetailView.as_view(), name='post-detail-page'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -10,9 +10,7 @@ class StartingPageView(ListView):
     context_object_name = 'posts'
 
     def get_queryset(self):
-        base_query = super().get_queryset()
-        latest_posts = base_query.order_by('-date')[:3]
-        return latest_posts
+        return super().get_queryset().order_by('-date')[:3]
 
 
 # def starting_page(request):
@@ -22,15 +20,13 @@ class StartingPageView(ListView):
 #                   )
 
 
-class PostsView(ListView):
+class AllPostsView(ListView):
     template_name = 'blog/all-posts.html'
     model = Post
     context_object_name = 'all_posts'
 
     def get_queryset(self):
-        base_query = super().get_queryset()
-        latest_posts = base_query.order_by('-date')[:3]
-        return latest_posts
+        return super().get_queryset().order_by('-date')
 
 
 # def posts(request):
@@ -43,19 +39,6 @@ class ReviewDetailView(DetailView):
     template_name = 'blog/post-detail.html'
     model = Post
 
-    # def get_context_data(self, **kwargs):
-    #         context = super().get_context_data(**kwargs)
-    #         loaded_review = self.object
-    #         request = self.request
-    #         favorite_id = request.session.get("favorite_review")
-    #         context["is_favorite"] = favorite_id == str(loaded_review.id)
-    #         return context
-
-
-# def post_detail(request, slug):
-#     # identified_post = Post.objects.get(slug=slug)
-#     identified_post = get_object_or_404(Post, slug=slug)
-#     return render(request, 'blog/post-detail.html',
-#                   {'post': identified_post,
-#                    'post_tags': identified_post.tag.all(),
-#                    })
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post_tags'] = self.object.tags.all()
