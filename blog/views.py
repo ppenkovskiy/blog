5c147967067efd10a50bb832f067e84b2221b460
Your Name <you@example.com>
2023-10-23 16:14:10 +0300

    23

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,61 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+
 from .models import Post
 
 
-def starting_page(request):
-    latest_posts = Post.objects.all().order_by('-date')[:3]
-    return render(request, "blog/index.html",
-                  {'posts': latest_posts}
-                  )
+class StartingPageView(ListView):
+    template_name = 'blog/index.html'
+    model = Post
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        base_query = super().get_queryset()
+        latest_posts = base_query.order_by('-date')[:3]
+        return latest_posts
 
 
-def posts(request):
-    all_posts = Post.objects.all().order_by('-date')
-    return render(request, "blog/all-posts.html",
-                  {'all_posts': all_posts}
-                  )
+# def starting_page(request):
+#     latest_posts = Post.objects.all().order_by('-date')[:3]
+#     return render(request, "blog/index.html",
+#                   {'posts': latest_posts}
+#                   )
 
 
-def post_detail(request, slug):
-    # identified_post = Post.objects.get(slug=slug)
-    identified_post = get_object_or_404(Post, slug=slug)
-    return render(request, 'blog/post-detail.html',
-                  {'post': identified_post,
-                          'post_tags': identified_post.tag.all(),
-                   })
+class PostsView(ListView):
+    template_name = 'blog/all-posts.html'
+    model = Post
+    context_object_name = 'all_posts'
+
+    def get_queryset(self):
+        base_query = super().get_queryset()
+        latest_posts = base_query.order_by('-date')[:3]
+        return latest_posts
+
+
+# def posts(request):
+#     all_posts = Post.objects.all().order_by('-date')
+#     return render(request, "blog/all-posts.html",
+#                   {'all_posts': all_posts}
+#                   )
+
+class ReviewDetailView(DetailView):
+    template_name = 'blog/post-detail.html'
+    model = Post
+
+    # def get_context_data(self, **kwargs):
+    #         context = super().get_context_data(**kwargs)
+    #         loaded_review = self.object
+    #         request = self.request
+    #         favorite_id = request.session.get("favorite_review")
+    #         context["is_favorite"] = favorite_id == str(loaded_review.id)
+    #         return context
+
+
+# def post_detail(request, slug):
+#     # identified_post = Post.objects.get(slug=slug)
+#     identified_post = get_object_or_404(Post, slug=slug)
+#     return render(request, 'blog/post-detail.html',
+#                   {'post': identified_post,
+#                    'post_tags': identified_post.tag.all(),
+#                    })
--- my_site/settings.py
+++ my_site/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-)nh=n!_yvsu=f^=3-lgiza8(aml^vg9i#q*od@98l_!ykyj)$^
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -72,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'my_site.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -82,7 +79,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -102,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -113,7 +108,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -127,3 +121,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = BASE_DIR / "uploads"
+MEDIA_URL = '/files/'
